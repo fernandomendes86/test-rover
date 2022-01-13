c41112d7edd9c24a8207d30cb48fa711b20c73ac
Fernando Mendes <fernando.msantos1@gmail.com>
2022-01-13 12:53:56 +0000

    Rover - turn right

--- lib/rover/rover.rb
+++ lib/rover/rover.rb
@@ -2,6 +2,7 @@ class Rover
  
   attr_accessor :position_x, :position_y, :guidance
   $hash_left = { 'N': 'W', 'W': 'S', 'S': 'E', 'E': 'N'}
+  $hash_right = { 'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'}
 
   def initialize(position_x, position_y, guidance)
     @position_x = position_x
@@ -11,6 +12,10 @@ class Rover
 
   def turn_left
     self.guidance = $hash_left[self.guidance.to_sym]  
+  end
+
+  def turn_right
+    self.guidance = $hash_right[self.guidance.to_sym]  
   end
   
 end
--- spec/rover/rover_spec.rb
+++ spec/rover/rover_spec.rb
@@ -7,20 +7,37 @@ describe Rover do
     @rover_b = Rover.new(3, 3, 'E')
   end 
 
-  it 'create rovers' do
+  it 'Create rovers A e B' do
     expect(@rover_a).to have_attributes(position_x: 1, position_y: 2, guidance: 'N')
     expect(@rover_b).to have_attributes(position_x: 3, position_y: 3, guidance: 'E')
   end
 
-  it 'rover A - turn left ' do
+  it 'Rover A - turn left' do
     @rover_a.turn_left
     expect(@rover_a).to have_attributes(guidance: 'W')
+
     @rover_a.turn_left
     expect(@rover_a).to have_attributes(guidance: 'S')
+    
     @rover_a.turn_left
     expect(@rover_a).to have_attributes(guidance: 'E')
+    
     @rover_a.turn_left
     expect(@rover_a).to have_attributes(guidance: 'N')
+  end
+
+  it 'Rover B - turn right' do
+    @rover_b.turn_right
+    expect(@rover_b).to have_attributes(guidance: 'S')
+    
+    @rover_b.turn_right
+    expect(@rover_b).to have_attributes(guidance: 'W')
+    
+    @rover_b.turn_right
+    expect(@rover_b).to have_attributes(guidance: 'N')
+    
+    @rover_b.turn_right
+    expect(@rover_b).to have_attributes(guidance: 'E')
   end
 
 end
