f5af5a63012f6448da8e985978f37be4817c03b8
Fernando Mendes <fernando.msantos1@gmail.com>
2022-01-13 17:26:46 +0000

    add test with rspec

--- spec/rover/rover_spec.rb
+++ spec/rover/rover_spec.rb
@@ -40,7 +40,7 @@ describe Rover do
     expect(@rover_b).to have_attributes(guidance: 'E')
   end
 
-  it 'Rover next position of N to W' do
+  it 'Rover - turn left - next position of N to W' do
     @rover_a.turn_left
     previous_postion_x = @rover_a.position_x
     @rover_a.next_position
@@ -48,12 +48,61 @@ describe Rover do
     expect(@rover_a.position_x).to eq(previous_postion_x - 1)
   end
 
-  it 'Rover next position of E to S' do
+  it 'Rover - turn left - next position of W to S' do
+    @rover_a.turn_left
+    previous_position_y = @rover_a.position_y
+    @rover_a.next_position
+    expect(@rover_a.guidance).to eq('S')
+    expect(@rover_a.position_y).to eq(previous_position_y - 1)
+  end
+
+  it 'Rover - turn left - next position of S to E' do
+    @rover_a.turn_left
+    previous_position_x = @rover_a.position_x
+    @rover_a.next_position
+    expect(@rover_a.guidance).to eq('E')
+    expect(@rover_a.position_x).to eq(previous_position_x + 1)
+  end
+
+  it 'Rover - turn left - next position of E to N' do
+    @rover_a.turn_left
+    previous_position_y = @rover_a.position_y
+    @rover_a.next_position
+    expect(@rover_a.guidance).to eq('N')
+    expect(@rover_a.position_y).to eq(previous_position_y + 1)
+  end
+
+  it 'Rover - turn right - next position of E to S' do
     @rover_b.turn_right
     previous_postion_y = @rover_b.position_y
     @rover_b.next_position
     expect(@rover_b.guidance).to eq('S')
     expect(@rover_b.position_y).to eq(previous_postion_y - 1)
   end
+
+  it 'Rover - turn right - next position of S to W' do
+    @rover_b.turn_right
+    previous_postion_x = @rover_b.position_x
+    @rover_b.next_position
+    expect(@rover_b.guidance).to eq('W')
+    expect(@rover_b.position_x).to eq(previous_postion_x - 1)
+  end
+
+  it 'Rover - turn right - next position of W to N ' do
+    @rover_b.turn_right
+    previous_postion_y = @rover_b.position_y
+    @rover_b.next_position
+    expect(@rover_b.guidance).to eq('N')
+    expect(@rover_b.position_y).to eq(previous_postion_y + 1)
+  end
+
+  it 'Rover - turn right - next position of N to E ' do
+    @rover_b.turn_right
+    previous_postion_x = @rover_b.position_x
+    @rover_b.next_position
+    expect(@rover_b.guidance).to eq('E')
+    expect(@rover_b.position_x).to eq(previous_postion_x + 1)
+  end
+
 
 end
